b9a591c5899e47d39c531989531bcb0e2efeda7f
beedybolade <boladebode@gmail.com>
2019-02-04 14:27:09 +0100

    New modification, development going on linux

--- software/classes/db.php
+++ software/classes/db.php
@@ -4,7 +4,7 @@ class Database
 { 
 public $db; 
 private static $dsn = 'mysql:host=localhost:3306;dbname=beedypos'; 
-private static $user= 'root'; 
+private static $user= 'beedy'; 
 private static $pass= 'kaydee'; 
 public static $instance; 
 public function __construct() 
